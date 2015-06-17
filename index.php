2eef6bf5b2f7c10959fa9abe0265e5a9b69453a9
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2015-06-16 22:24:49 -0300

    Added an example of quadratic complexity.

--- index.php
+++ index.php
@@ -15,6 +15,20 @@ var_dump($foo->get("whattheheck"));
 
 $main = new \Introduction\DataStructures\QuadraticComplexity\Main();
 
-for ($i = 0; $i < 9; $i++) {
-    
+for ($i = 0; $i < 10; $i++) {
+    $node = new \Introduction\DataStructures\QuadraticComplexity\Node();
+    $node->setElement($i);
+
+    $main->set('element'.$i, $node);
+}
+
+foreach($main->getElements() as $element) {
+    foreach($element as $item) {
+        // A quadratic complexity can be found where you have an loop over another loop. 
+        // This makes that if the first loop have 10 itens, and each of these itens have
+        // 10 another itens, you'll find yourself in a complexity of 10 * 10 = 100 iterations!
+        //
+        // Of course that this is a simple example, but it suits the idea.
+        var_dump($item);
+    }
 }
