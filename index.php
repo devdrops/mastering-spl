6cf0ea6b9004d47ea22f0f0f208dbad2927a8c2a
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2015-06-19 20:57:05 -0300

    LIFO and FIFO examples.

--- index.php
+++ index.php
@@ -13,9 +13,9 @@ foreach($foo as $element) {
     var_dump($element);
 }
 
-echo 'ITERATOR MODE: LIFO'.PHP_EOL;
+echo 'ITERATOR MODE: LIFO|IT_MODE_KEEP'.PHP_EOL;
 $foo = new \SplDoublyLinkedList();
-$foo->setIteratorMode(IT_MODE_LIFO);
+$foo->setIteratorMode($foo::IT_MODE_LIFO);
 for ($i = 0; $i < 20; $i++) {
     $foo->add($i, 'elem'.$i);
 }
@@ -23,12 +23,23 @@ foreach($foo as $element) {
     var_dump($element);
 }
 
-echo 'ITERATOR MODE: FIFO'.PHP_EOL;
+echo 'ITERATOR MODE: FIFO|IT_MODE_KEEP'.PHP_EOL;
 $foo = new \SplDoublyLinkedList();
-$foo->setIteratorMode(IT_MODE_FIFO);
+$foo->setIteratorMode($foo::IT_MODE_FIFO);
 for ($i = 0; $i < 20; $i++) {
     $foo->add($i, 'elem'.$i);
 }
 foreach($foo as $element) {
     var_dump($element);
 }
+
+echo 'ITERATOR MODE: FIFO|IT_MODE_DELETE'.PHP_EOL;
+$foo = new \SplDoublyLinkedList();
+$foo->setIteratorMode($foo::IT_MODE_FIFO|$foo::IT_MODE_DELETE);
+for ($i = 0; $i < 20; $i++) {
+    $foo->add($i, 'elem'.$i);
+}
+foreach($foo as $element) {
+    var_dump($element);
+}
+print_r('COUNT: '.$foo->count().PHP_EOL);
