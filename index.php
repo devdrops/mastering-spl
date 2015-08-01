952a90940735d2b6d42f183da1ad20a0d1d672ec
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2015-08-01 17:52:10 -0300

    Queue start

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+/**
+ * The SplQueue class
+ * 
+ * - Curious:
+ *		SplStack()->setIteratorMode(\SplDoublyLinkedList::IT_MODE_LIFO)
+ */
+
+$queue = new \SplQueue();
+
+$queue->setIteratorMode(\SplDoublyLinkedList::IT_MODE_FIFO);
