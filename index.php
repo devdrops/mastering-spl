26c5b1255555be13d41a6f07476f2a75fd19fe30
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2015-08-01 18:01:51 -0300

    Enqueue() and dequeue().

--- index.php
+++ index.php
@@ -5,10 +5,35 @@ require_once __DIR__ . '/vendor/autoload.php';
 /**
  * The SplQueue class
  * 
- * - Curious:
- *		SplStack()->setIteratorMode(\SplDoublyLinkedList::IT_MODE_LIFO)
+ * - Just like the SplStack class, we can find the same bug: once 
+ *   you've already added values to the queue, you can change the
+ *   iterator mode from FIFO to LIFO. Geez...
  */
 
 $queue = new \SplQueue();
 
 $queue->setIteratorMode(\SplDoublyLinkedList::IT_MODE_FIFO);
+for ($i = 0; $i < 10; $i++) {
+    $queue->enqueue('elem'.$i);
+}
+foreach($queue as $element) {
+    var_dump($element);
+}
+
+for ($i = 0; $i < 10; $i++) {
+    $queue->dequeue();
+}
+foreach($queue as $element) {
+    var_dump($element);
+}
+
+/*
+$queue->setIteratorMode(\SplDoublyLinkedList::IT_MODE_LIFO);
+for ($i = 0; $i < 10; $i++) {
+    $queue->enqueue('elem'.$i);
+}
+foreach($queue as $element) {
+    var_dump($element);
+}
+*/
+
