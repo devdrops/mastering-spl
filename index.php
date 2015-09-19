65300026fb159219d295a4f006524cf2b9a297a2
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2015-09-19 11:44:30 -0300

    SplHeap examples

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+$data = range(0, 20);
+shuffle($data);
+
---
+++ src/Spl/Example/Heap.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Spl\Example;
+
+class Heap extends \SplHeap
+{
+    protected function compare($value1, $value2)
+    {
+        if ($value1 > $value2) {
+            return 1;
+        }
+        
+        if ($value1 < $value2) {
+            return -1;
+        }
+        
+        if ($value1 == $value2) {
+            return 0;
+        }
+        
+    }
+}
